4e53ada5d015d0e1f7690936887acac15e16fc59
Ivan Fuentes <iv.ifa94@gmail.com>
2024-03-22 15:59:42 +0100

    added projects preview

--- src/components/body/projects/ProjectCard.jsx
+++ src/components/body/projects/ProjectCard.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
-const ProjectCard = ({ uri, img }) => {
+const ProjectCard = ({ children: text, uri, img, title }) => {
   return (
     <div className="mx-4 my-10 overflow-hidden flex flex-col items-center border-white border-2 rounded-xl shadow shadow-snow hover:shadow-lg hover:shadow-stone-300 transition ease-in-out delay-250 hover:scale-105 ">
       <div className="w-full">
-        <div className="z-0 text-center bg-teal-700">Title</div>
-        <img src="http://placehold.co/600x200" alt="" />
+        <div className="z-0 text-center bg-teal-700">{title}</div>
+        <a href={uri}>
+          <img src={img} alt="" />
+        </a>
       </div>
-
-      <div className="my-2 mx-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi est
-        placeat accusamus fugit quibusdam praesentium?
-      </div>
+      <p>{text}</p>
     </div>
   );
 };
--- src/components/body/projects/Projects.jsx
+++ src/components/body/projects/Projects.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
+import img1 from "../../../assets/project1.png";
+import img2 from "../../../assets/project2.png";
 
 const Projects = () => {
   return (
     <section id="Projects">
       <h2 className="text-center text-6xl">Projects</h2>
       <div className="my-8 flex gap-x-8 gap-y-4">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        <ProjectCard
+          uri={"https://lovecraftapirest.fly.dev/"}
+          img={img1}
+          title={"Lovecraftian Api"}
+        >
+          A Lovecraftian universe beasts API with description, images, author and more.
+        </ProjectCard>
+        <ProjectCard
+          uri={"https://lovecraftbestiary.fly.dev/"}
+          img={img2}
+          title={"Lovecraftian Bestiary"}
+        >
+          A website made to display the beasts images from the the Lovecraftian API and miscellaneous.
+        </ProjectCard>
       </div>
     </section>
   );
